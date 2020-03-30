ff5d8b73afe5486d92f78a65c2027ce932f7a450
evandrogouveia <vando_gouveia@hotmail.com>
2020-03-30 11:26:07 -0300

    enviando arquivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef, ViewChildren, AfterViewInit, QueryList } from '@angular/core';
+import { SocketIoService } from './socket-io.service';
+import { Message } from './message';
+import { Subscription } from 'rxjs';
+import { MatList, MatListItem } from '@angular/material/list';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
+  nickname: string;
+  message: string;
+  messages: Message[] = [];
+
+  private subscriptionMessages: Subscription;
+  private subscriptionList: Subscription;
+
+  @ViewChild(MatList, {read: ElementRef}) list: ElementRef;
+  @ViewChildren(MatListItem) listItems: QueryList<MatListItem>;
+
+  constructor( private socketService: SocketIoService) {}
+
+  ngOnInit() {
+    this.subscriptionMessages = this.socketService.messages()
+      .subscribe((m: Message) => {
+        console.log(m);
+        this.messages.push(m);
+      });
+  }
+
+  ngAfterViewInit() {
+    this.subscriptionList =  this.listItems.changes.subscribe((e) => {
+      this.list.nativeElement.scrollTop = this.list.nativeElement.scrollHeight;
+    });
+  }
+
+  send() {
+    this.socketService.send({
+      from: this.nickname,
+      message: this.message
+    });
+    this.message = '';
+  }
+
+  ngOnDestroy() {
+    this.subscriptionMessages.unsubscribe();
+    this.subscriptionList.unsubscribe();
+  }
 }
